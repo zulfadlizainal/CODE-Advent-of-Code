e25473e69b32d3c351ae2ba0464b9bbb709b29a1
Zulfadli Zainal <50709439+zulfadlizainal@users.noreply.github.com>
2020-12-12 17:29:41 +0900

    2020_Day2

--- 2020/Day2/day2_part2.py
+++ 2020/Day2/day2_part2.py
@@ -8,7 +8,7 @@ for index in data.readlines():
     policy_min, policy_max = map(int,index[0].split('-'))
     key = index[1][0]
     
-    if index[2].count(key) >= policy_min and index[2].count(key) <= policy_max:
+    if (index[2][policy_min-1] == key) ^ (index[2][policy_max-1] == key):
         result = result + 1
     
 print(result)
